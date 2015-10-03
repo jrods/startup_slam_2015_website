b27fcc3bfca681d8ef3932697cc73f728e9df469
Ryan <ryan.samarajeewa@gmail.com>
2015-10-03 14:07:39 -0700

    Add genRandString() function

--- js/main.js
+++ js/main.js
@@ -2,3 +2,17 @@ $(function() {
     // Put any custom JS here
     console.log( "Scanning for viruses..." );
 });
+
+function genRandString(){
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for( var i=0; i < 5; i++ )
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
+function cycleHints(){
+
+}
