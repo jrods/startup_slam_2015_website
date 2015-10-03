edc9d50d4ddcd13bb7d903a64abd038db0c090ea
Ryan <ryan.samarajeewa@gmail.com>
2015-10-03 14:20:08 -0700

    Implement cycleHints() to input and textarea

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ $(function() {
     console.log( "Scanning for viruses..." );
 });
 
-function genRandString(){
+function genRandString(num){
   var text = "";
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  for( var i=0; i < 15; i++ )
+  for( var i=0; i < num; i++ )
       text += possible.charAt(Math.floor(Math.random() * possible.length));
 
   return text;
@@ -19,6 +19,7 @@ $(document).ready(function(){
 
 function cycleHints(){
   window.setInterval(function(){
-    $("textarea").attr("placeholder", genRandString());
+    $("textarea").attr("placeholder", genRandString(150));
+    $("input").attr("placeholder", genRandString(50));
   }, 100);
 }
