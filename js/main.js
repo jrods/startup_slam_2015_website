13669e49d3dc7f5dbbaea3923c2f8f3686770f63
Ryan <ryan.samarajeewa@gmail.com>
2015-10-03 14:15:58 -0700

    Add cycleHints() function

--- js/main.js
+++ js/main.js
@@ -7,12 +7,18 @@ function genRandString(){
   var text = "";
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  for( var i=0; i < 5; i++ )
+  for( var i=0; i < 15; i++ )
       text += possible.charAt(Math.floor(Math.random() * possible.length));
 
   return text;
 }
 
-function cycleHints(){
+$(document).ready(function(){
+  cycleHints();
+});
 
+function cycleHints(){
+  window.setInterval(function(){
+    $("textarea").attr("placeholder", genRandString());
+  }, 100);
 }
