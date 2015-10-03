ad864fb03dd3858ce816d2ee5559f3bf539ac142
Andy <tehandyyang@gmail.com>
2015-10-03 14:20:09 -0700

    Redirects onclick for "Send" button

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@ $(function() {
     // Put any custom JS here
     console.log( "Scanning for viruses..." );
 });
+
+function redirect(){
+	window.location.href="https://www.youtube.com/watch?v=QH2-TGUlwu4"; 
+
+}
